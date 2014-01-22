8975315a72ac22d95e6c4f969637b5a7faa91e59
cstadler <cstadler@informatik.uni-leipzig.de>
2014-01-22 19:55:11 +0100

    Added hacky property inference

--- cstadler-tools-debian-cli/src/deb/resources/usr/bin/cst-virtload-list.sh
+++ cstadler-tools-debian-cli/src/deb/resources/usr/bin/cst-virtload-list.sh
@@ -8,6 +8,7 @@ virtPass="$3"
 httpUser="$4"
 httpPass="$5"
 
+virtIsql='isql-vt'
 
 while read url graphName; do
 
@@ -27,6 +28,10 @@ while read url graphName; do
     echo "$fileName -> $graphName"
 
     cst-virtload.sh "$fileName" "$graphName" "$virtPort" "$virtUser" "$virtPass"
+
+    propInsertStmt="EXEC=Sparql Insert Into <$graphName> { ?p a <http://www.w3.org/1999/02/22-rdf-syntax-ns#Property> . } From <$graphName> { ?s ?p ?o }"
+
+    "$virtIsql" "$virtPort" "$virtUser" "$virtPass" "$propInsertStmt"
 done
 
 #wget  --http-user="$httpUser" --http-password="$httpPass" http://data.geoknow.eu/internal/demo/sparqlify/2014-01-21-unister-hotel-reviews.sparqlify.sorted.nt.bz2
