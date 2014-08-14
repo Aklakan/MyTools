ea583c403f7e756076badaf7f3db96b80e141c27
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-14 20:21:06 +0200

    Style fixes

--- cstadler-tools-debian-cli/src/deb/resources/usr/share/cstadler-tools/nodejs/createIndex.js
+++ cstadler-tools-debian-cli/src/deb/resources/usr/share/cstadler-tools/nodejs/createIndex.js
@@ -1,24 +1,23 @@
-
+console.log('\'use strict\';');
+console.log();
 console.log('var ns = {');
 
 require('file').walkSync('.', function(dir, path, fileNames) {
   var entries = fileNames
-    .filter(function(fileName) { return fileName != 'index.js'; })
+    .filter(function(fileName) { return fileName.match('\.js$') && fileName != 'index.js'; })
     .map(function(fileName) {
-      if(fileName.match('\.js$')) {
-        var className = fileName.substring(0, fileName.length - 3);
-        var requirePath = './' + path.join('/') + className;
+      var className = fileName.substring(0, fileName.length - 3);
+      var requirePath = './' + path.join('/') + className;
 
-        return '  ' + className + ': require(\'' + requirePath + '\')';
-      }
+      return '    ' + className + ': require(\'' + requirePath + '\')';
     });
   var refs = entries.join(',\n');
   console.log(refs);
 });
 
 console.log('};');
-
+console.log();
 console.log('Object.freeze(ns);');
-
+console.log();
 console.log('module.exports = ns;');
-
+console.log();
