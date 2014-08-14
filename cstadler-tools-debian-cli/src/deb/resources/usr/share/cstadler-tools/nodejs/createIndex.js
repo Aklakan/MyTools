156bf0ab5f0bee243ec65f6efc13a1b0123179ea
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-14 20:36:51 +0200

    sigh... maybe now

--- cstadler-tools-debian-cli/src/deb/resources/usr/share/cstadler-tools/nodejs/createIndex.js
+++ cstadler-tools-debian-cli/src/deb/resources/usr/share/cstadler-tools/nodejs/createIndex.js
@@ -2,20 +2,20 @@ console.log('\'use strict\';');
 console.log();
 console.log('var ns = {');
 
+var entries = [];
 require('file').walkSync('.', function(dirPath, dirNames, fileNames) {
-console.log(arguments);
-  var entries = fileNames
+  fileNames
     .filter(function(fileName) { return fileName.match('\.js$') && fileName != 'index.js'; })
-    .map(function(fileName) {
+    .forEach(function(fileName) {
       var className = fileName.substring(0, fileName.length - 3);
       var requirePath = './' + (dirPath === '.' ? '' :  dirPath + '/') + className;
 
-      return '    ' + className + ': require(\'' + requirePath + '\')';
+      entries.push('    ' + className + ': require(\'' + requirePath + '\')');
     });
-  var refs = entries.join(',\n');
-  console.log(refs);
 });
 
+var refs = entries.join(',\n');
+console.log(refs);
 console.log('};');
 console.log();
 console.log('Object.freeze(ns);');
